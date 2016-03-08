48a936a24c63b07d0144609fc7ce790f7a55f12a
benhowarth <benhowarthmail@gmail.com>
2016-03-08 00:16:29 +0000

    0.0.7
    Changed to weather underground

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-    APPID="GMaBrS50";
-    DEG="c";
+    APPID="70523fecced5a06b";
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(locationSuccess,locationError);
     }
@@ -11,35 +10,17 @@ $(document).ready(function(){
         lat=position.coords.latitude;
         lon=position.coords.longitude;
         console.log(lat,lon);
-        currentURL = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=44db6a862fba0b067b1930da0d769e98&units=metric";
+        currentURL="http://api.wunderground.com/api/"+APPID+"/conditions/q/"+lat+","+lon+".json";
+        forecastURL="http://api.wunderground.com/api/"+APPID+"/hourly10day/q/"+lat+","+lon+".json";
         
         $.getJSON(currentURL,function(r){
             results=r;
             console.log(r);
-            console.log(r.name);
-            console.log(r.weather[0].description);
-            console.log(r.main.temp+"°C");
-            console.log(r.main.humidity+"%");
-            console.log(r.wind.speed+"m/s");
-            console.log(r.dt);
         });
-        forecastURL="http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=44db6a862fba0b067b1930da0d769e98&units=metric";
         
         $.getJSON(forecastURL,function(r){
             results=r;
             console.log(r);
-            console.log(r.city.name);
-            tempStr="";
-            descStr="";
-            dtStr="";
-            for(i=0;i<r.list.length;i++){
-                tempStr+=r.list[i].main.temp+", ";
-                descStr+=r.list[i].weather[0].description+", ";
-                dtStr+=r.list[i].dt+", ";
-            }
-            console.log(tempStr);
-            console.log(descStr);
-            console.log(dtStr);
         });
         
     }
